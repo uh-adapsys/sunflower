ce5d21651365ba3c1c31bd3f070dc921761e5e72
Nathan Burke <natbur@gmail.com>
2013-03-19 13:31:15 +0000

    add eclipse project for sf_teleop modify teleop_joy to use new sf_teleop node

--- sf_teleop/src/p2os_teleop.cc
+++ sf_teleop/src/p2os_teleop.cc
@@ -47,7 +47,6 @@
  *       MA 02110-1301, USA.
  */
 
-
 #include <cstdlib>
 #include <unistd.h>
 #include <math.h>
@@ -55,175 +54,172 @@
 #include "sensor_msgs/Joy.h"
 #include "geometry_msgs/Twist.h"
 
-class TeleopBase 
-{
-   public:
-  geometry_msgs::Twist cmd, passthrough_cmd;
-  double req_vx, req_vy, req_vw;
-  double max_vx, max_vy, max_vw, max_vx_run, max_vy_run, max_vw_run;
-  int axis_vx, axis_vy, axis_vw;
-  int deadman_button, run_button;
-  bool deadman_no_publish_;
-  bool deadman_;
+class TeleopBase {
+public:
+	geometry_msgs::Twist cmd, passthrough_cmd;
+	double req_vw, req_vx, req_vy;
+	double max_vx, max_vy, max_vw, max_vx_run, max_vy_run, max_vw_run;
+	int axis_vw, axis_vx, axis_vy;
+	int run_button, deadman_button;
+	bool deadman_no_publish_;
+	bool deadman_;
 	bool running_;
 
-  ros::Time last_recieved_joy_message_time_;
-  ros::Duration joy_msg_timeout_;
+	ros::Time last_recieved_joy_message_time_;
+	ros::Duration joy_msg_timeout_;
 
-  ros::NodeHandle n_;
-  ros::Publisher vel_pub_;
-  ros::Subscriber joy_sub_;
-  ros::Subscriber passthrough_sub_;
+	ros::NodeHandle n_;
+	ros::Publisher vel_pub_;
+	ros::Subscriber joy_sub_;
+	ros::Subscriber passthrough_sub_;
 
-  TeleopBase(bool deadman_no_publish = false) : max_vx(0.6), max_vy(0.6), max_vw(0.8), max_vx_run(0.6), max_vy_run(0.6), max_vw_run(0.8), deadman_no_publish_(deadman_no_publish), running_(false)
-  { }
-
-  void init()
-      {
-        cmd.linear.x = cmd.linear.y = cmd.angular.z = 0;
-        n_.param("max_vx", max_vx, max_vx);
-        n_.param("max_vy", max_vy, max_vy);
-        n_.param("max_vw", max_vw, max_vw);
-        
-        // Set max speed while running
-        n_.param("max_vx_run", max_vx_run, max_vx_run);
-        n_.param("max_vy_run", max_vy_run, max_vy_run);
-        n_.param("max_vw_run", max_vw_run, max_vw_run);
-
-        n_.param("axis_vx", axis_vx, 3);
-        n_.param("axis_vw", axis_vw, 0);
-        n_.param("axis_vy", axis_vy, 2);
-        
-        n_.param("deadman_button", deadman_button, 0);
-        n_.param("run_button", run_button, 0);
-
-	double joy_msg_timeout;
-        n_.param("joy_msg_timeout", joy_msg_timeout, -1.0); //default to no timeout
-	if (joy_msg_timeout <= 0)
-	  {
-	    joy_msg_timeout_ = ros::Duration().fromSec(9999999);//DURATION_MAX;
-	    ROS_DEBUG("joy_msg_timeout <= 0 -> no timeout");
-	  }
-	else
-	  {
-	    joy_msg_timeout_.fromSec(joy_msg_timeout);
-	    ROS_DEBUG("joy_msg_timeout: %.3f", joy_msg_timeout_.toSec());
-	  }
-
-        ROS_DEBUG("max_vx: %.3f m/s\n", max_vx);
-        ROS_DEBUG("max_vy: %.3f m/s\n", max_vy);
-        ROS_DEBUG("max_vw: %.3f deg/s\n", max_vw*180.0/M_PI);
-        
-        ROS_DEBUG("max_vx_run: %.3f m/s\n", max_vx_run);
-        ROS_DEBUG("max_vy_run: %.3f m/s\n", max_vy_run);
-        ROS_DEBUG("max_vw_run: %.3f deg/s\n", max_vw_run*180.0/M_PI);
-        
-        ROS_DEBUG("axis_vx: %d\n", axis_vx);
-        ROS_DEBUG("axis_vy: %d\n", axis_vy);
-        ROS_DEBUG("axis_vw: %d\n", axis_vw);
-
-        
-        ROS_INFO("deadman_button: %d", deadman_button);
-        ROS_INFO("run_button: %d", run_button);
-        ROS_DEBUG("joy_msg_timeout: %f\n", joy_msg_timeout);
-        
-        vel_pub_ = n_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
-        passthrough_sub_ = n_.subscribe( "des_vel", 10, &TeleopBase::passthrough_cb, this );
-        joy_sub_ = n_.subscribe("joy", 10, &TeleopBase::joy_cb, this);
-
- 
+	TeleopBase(bool deadman_no_publish = false) :
+			max_vx(0.6), max_vy(0.6), max_vw(0.8), max_vx_run(0.6), max_vy_run(
+					0.6), max_vw_run(0.8), deadman_no_publish_(
+					deadman_no_publish), running_(false), axis_vx(0), axis_vy(
+					1), axis_vw(2), deadman_(false), deadman_button(5), run_button(
+					4), req_vw(0), req_vx(0), req_vy(0) {
 	}
-  
-	~TeleopBase() {  }
 
-	void passthrough_cb( const geometry_msgs::TwistConstPtr& pass_msg )
-	{
-		ROS_DEBUG( "passthrough_cmd: [%f,%f]", passthrough_cmd.linear.x, passthrough_cmd.angular.z );
+	void init() {
+		cmd.linear.x = cmd.linear.y = cmd.angular.z = 0;
+		n_.param("max_vx", max_vx, max_vx);
+		n_.param("max_vy", max_vy, max_vy);
+		n_.param("max_vw", max_vw, max_vw);
+
+		// Set max speed while running
+		n_.param("max_vx_run", max_vx_run, max_vx_run);
+		n_.param("max_vy_run", max_vy_run, max_vy_run);
+		n_.param("max_vw_run", max_vw_run, max_vw_run);
+
+		n_.param("axis_vx", axis_vx, 3);
+		n_.param("axis_vw", axis_vw, 0);
+		n_.param("axis_vy", axis_vy, 2);
+
+		n_.param("deadman_button", deadman_button, 0);
+		n_.param("run_button", run_button, 0);
+
+		double joy_msg_timeout;
+		n_.param("joy_msg_timeout", joy_msg_timeout, -1.0); //default to no timeout
+		if (joy_msg_timeout <= 0) {
+			joy_msg_timeout_ = ros::Duration().fromSec(9999999); //DURATION_MAX;
+			ROS_DEBUG("joy_msg_timeout <= 0 -> no timeout");
+		} else {
+			joy_msg_timeout_.fromSec(joy_msg_timeout);
+			ROS_DEBUG("joy_msg_timeout: %.3f", joy_msg_timeout_.toSec());
+		}
+
+		ROS_DEBUG("max_vx: %.3f m/s\n", max_vx);
+		ROS_DEBUG("max_vy: %.3f m/s\n", max_vy);
+		ROS_DEBUG("max_vw: %.3f deg/s\n", max_vw * 180.0 / M_PI);
+
+		ROS_DEBUG("max_vx_run: %.3f m/s\n", max_vx_run);
+		ROS_DEBUG("max_vy_run: %.3f m/s\n", max_vy_run);
+		ROS_DEBUG("max_vw_run: %.3f deg/s\n", max_vw_run * 180.0 / M_PI);
+
+		ROS_DEBUG("axis_vx: %d\n", axis_vx);
+		ROS_DEBUG("axis_vy: %d\n", axis_vy);
+		ROS_DEBUG("axis_vw: %d\n", axis_vw);
+
+		ROS_INFO("deadman_button: %d", deadman_button);
+		ROS_INFO("run_button: %d", run_button);
+		ROS_DEBUG("joy_msg_timeout: %f\n", joy_msg_timeout);
+
+		vel_pub_ = n_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+		passthrough_sub_ = n_.subscribe("des_vel", 10,
+				&TeleopBase::passthrough_cb, this);
+		joy_sub_ = n_.subscribe("joy", 10, &TeleopBase::joy_cb, this);
+
+	}
+
+	~TeleopBase() {
+	}
+
+	void passthrough_cb(const geometry_msgs::TwistConstPtr& pass_msg) {
+		ROS_DEBUG(
+				"passthrough_cmd: [%f,%f]", passthrough_cmd.linear.x, passthrough_cmd.angular.z);
 		passthrough_cmd = *pass_msg;
 	}
 
-	void joy_cb(const sensor_msgs::Joy::ConstPtr& joy_msg)
-	{
-        
-    deadman_ = (((unsigned int)deadman_button < joy_msg->get_buttons_size()) && joy_msg->buttons[deadman_button]);
+	void joy_cb(const sensor_msgs::Joy::ConstPtr& joy_msg) {
 
-    if (!deadman_)
-    	return;
-		
+		deadman_ =
+				(((unsigned int) deadman_button < joy_msg->get_buttons_size())
+						&& joy_msg->buttons[deadman_button]);
+
+		if (!deadman_)
+			return;
+
 		//Record this message reciept
 		last_recieved_joy_message_time_ = ros::Time::now();
 
-    // Base
-    running_ = (((unsigned int)run_button < joy_msg->get_buttons_size()) && joy_msg->buttons[run_button]);
-    double vx = running_ ? max_vx_run : max_vx;
-    double vy = running_ ? max_vy_run : max_vy;
-    double vw = running_ ? max_vw_run : max_vw;
+		// Base
+		running_ = (((unsigned int) run_button < joy_msg->get_buttons_size())
+				&& joy_msg->buttons[run_button]);
+		double vx = running_ ? max_vx_run : max_vx;
+		double vy = running_ ? max_vy_run : max_vy;
+		double vw = running_ ? max_vw_run : max_vw;
 
-    if((axis_vx >= 0) && (((unsigned int)axis_vx) < joy_msg->get_axes_size()))
-    	req_vx = joy_msg->axes[axis_vx] * vx;
-    else
-      req_vx = 0.0;
-    if((axis_vy >= 0) && (((unsigned int)axis_vy) < joy_msg->get_axes_size()))
-    	req_vy = joy_msg->axes[axis_vy] * vy;
-    else
-      req_vy = 0.0;
-    if((axis_vw >= 0) && (((unsigned int)axis_vw) < joy_msg->get_axes_size()))
-      req_vw = joy_msg->axes[axis_vw] * vw;
-    else
-      req_vw = 0.0;
+		if ((axis_vx >= 0)
+				&& (((unsigned int) axis_vx) < joy_msg->get_axes_size()))
+			req_vx = joy_msg->axes[axis_vx] * vx;
+		else
+			req_vx = 0.0;
+		if ((axis_vy >= 0)
+				&& (((unsigned int) axis_vy) < joy_msg->get_axes_size()))
+			req_vy = joy_msg->axes[axis_vy] * vy;
+		else
+			req_vy = 0.0;
+		if ((axis_vw >= 0)
+				&& (((unsigned int) axis_vw) < joy_msg->get_axes_size()))
+			req_vw = joy_msg->axes[axis_vw] * vw;
+		else
+			req_vw = 0.0;
 
-  }
-
-  void send_cmd_vel()
-  {
-    if(deadman_ && last_recieved_joy_message_time_ + joy_msg_timeout_ > ros::Time::now() && running_ )
-    {
-	cmd.linear.x = req_vx;
-	cmd.linear.y = req_vy;
-	cmd.angular.z = req_vw;
-	vel_pub_.publish(cmd);
-         
-	fprintf(stdout,"teleop_base:: %f, %f, %f\n",cmd.linear.x,cmd.linear.y,cmd.angular.z);
-    }
-    else
-    {
-	ROS_INFO("Running_ %d\n", running_);
-	ROS_INFO("Deadman_ %d\n", deadman_);
-	ROS_INFO("Msgtime %s\n", last_recieved_joy_message_time_);
-	ROS_INFO("Timeout %s\n", joy_msg_timeout_);
-	cmd = passthrough_cmd;
-	{
-	   vel_pub_.publish(cmd);
 	}
-    }
-  }
+
+	void send_cmd_vel() {
+		if (deadman_
+				&& last_recieved_joy_message_time_ + joy_msg_timeout_
+						> ros::Time::now() && running_) {
+			cmd.linear.x = req_vx;
+			cmd.linear.y = req_vy;
+			cmd.angular.z = req_vw;
+			vel_pub_.publish(cmd);
+
+			fprintf(stdout, "teleop_base:: %f, %f, %f\n", cmd.linear.x,
+					cmd.linear.y, cmd.angular.z);
+		} else {
+			cmd = passthrough_cmd;
+			{
+				vel_pub_.publish(cmd);
+			}
+		}
+	}
 };
 
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "teleop_base");
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "teleop_base");
 	ros::NodeHandle nh;
-  const char* opt_no_publish    = "--deadman_no_publish";
-  
-  bool no_publish = false;
-  for(int i=1;i<argc;i++)
-  {
-    if(!strncmp(argv[i], opt_no_publish, strlen(opt_no_publish)))
-      no_publish = true;
-  }
+	const char* opt_no_publish = "--deadman_no_publish";
 
-  ros::Rate pub_rate(20);
+	bool no_publish = false;
+	for (int i = 1; i < argc; i++) {
+		if (!strncmp(argv[i], opt_no_publish, strlen(opt_no_publish)))
+			no_publish = true;
+	}
 
-  TeleopBase teleop_base(no_publish);
-  teleop_base.init();
-  
-  while (ros::ok())
-  {
-    ros::spinOnce(); 
-    teleop_base.send_cmd_vel();
-    pub_rate.sleep();
-  }
-  
-  exit(0);
-  return 0;
+	ros::Rate pub_rate(20);
+
+	TeleopBase teleop_base(no_publish);
+	teleop_base.init();
+
+	while (ros::ok()) {
+		ros::spinOnce();
+		teleop_base.send_cmd_vel();
+		pub_rate.sleep();
+	}
+
+	exit(0);
+	return 0;
 }
