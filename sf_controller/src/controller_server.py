b6ad7b4dfac97cdd88c27188946098ad405adc1a
Nathan Burke <n.burke@natbur.com>
2015-02-19 15:52:53 +0000

    attempt to fix parallelisation problem

--- sf_controller/src/controller_server.py
+++ sf_controller/src/controller_server.py
@@ -5,23 +5,23 @@ Created on 12 Mar 2013
 
 @author: nathan
 '''
+from threading import Thread
+import math
+import time
+
 import roslib
 roslib.load_manifest('sf_controller')
 
-import time
-import math
-from threading import Thread
-
-import rospy
-import actionlib
-import sf_controller_msgs.msg
-import sf_lights_msgs.msg
 from dynamixel_msgs.msg import JointState
 from geometry_msgs.msg import PoseStamped, Twist
-from p2os_driver.msg import MotorState
-from tf.transformations import quaternion_from_euler
-from std_msgs.msg import Float64
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from p2os_driver.msg import MotorState
+from std_msgs.msg import Float64
+from tf.transformations import quaternion_from_euler
+import actionlib
+import rospy
+import sf_controller_msgs.msg
+import sf_lights_msgs.msg
 
 
 class SunflowerAction(object):
@@ -30,7 +30,8 @@ class SunflowerAction(object):
 
     def __init__(self, name):
         self._action_name = name
-        self._as = actionlib.SimpleActionServer(self._action_name, sf_controller_msgs.msg.SunflowerAction, execute_cb=self.execute_cb, auto_start=False)
+        self._as = actionlib.SimpleActionServer(
+            self._action_name, sf_controller_msgs.msg.SunflowerAction, execute_cb=self.execute_cb, auto_start=False)
         self._as.start()
         rospy.loginfo("Started Sunflower Controller ActionServer")
         self._pubs = self._connectToJoints()
@@ -69,10 +70,11 @@ class SunflowerAction(object):
                     for jointName in value['joint_names']:
                         topic = jointName + '_controller'
                         if topic not in pubs:
-                            pubs[topic] = rospy.Publisher(topic + '/command', Float64)
+                            pubs[topic] = rospy.Publisher(
+                                topic + '/command', Float64)
         else:
             rospy.logerr("%s:  No joints received from parameter server",
-                          self._action_name)
+                         self._action_name)
 
         return pubs
 
@@ -109,10 +111,11 @@ class SunflowerAction(object):
 
     def stop(self, name):
         rospy.loginfo("%s: Stopping %s",
-            self._action_name,
-            name)
+                      self._action_name,
+                      name)
         if name == 'base':
-            client = actionlib.SimpleActionClient('/lights', sf_lights_msgs.msg.LightsAction)
+            client = actionlib.SimpleActionClient(
+                '/lights', sf_lights_msgs.msg.LightsAction)
             client.wait_for_server()
             client.cancel_all_goals()
         elif name == 'light':
@@ -125,7 +128,8 @@ class SunflowerAction(object):
         self._as.set_succeeded(self._result)
 
     def setlight(self, color):
-        client = actionlib.SimpleActionClient('/lights', sf_lights_msgs.msg.LightsAction)
+        client = actionlib.SimpleActionClient(
+            '/lights', sf_lights_msgs.msg.LightsAction)
         client.wait_for_server()
         goal = sf_lights_msgs.msg.LightsGoal(rgb=color)
         handle = _ActionHandle(client)
@@ -136,15 +140,16 @@ class SunflowerAction(object):
     def move(self, goal):
         joints = goal.jointPositions
 
-        if(goal.namedPosition != '' and goal.namedPosition != None):
-            param = '/sf_controller/' + goal.component + '/' + goal.namedPosition
+        if(goal.namedPosition != '' and goal.namedPosition is not None):
+            param = '/sf_controller/' + \
+                goal.component + '/' + goal.namedPosition
             if(rospy.has_param(param)):
                 joints = rospy.get_param(param)[0]
 
         rospy.loginfo("%s: Setting %s to %s",
-                self._action_name,
-                goal.component,
-                goal.namedPosition or joints)
+                      self._action_name,
+                      goal.component,
+                      goal.namedPosition or joints)
 
         if goal.component == 'base':
             result = self.navigate(goal, joints)
@@ -154,10 +159,10 @@ class SunflowerAction(object):
             result = self.moveJoints(goal, joints)
 
         rospy.logdebug("%s: '%s to %s' Result:%s",
-                self._action_name,
-                goal.component,
-                goal.namedPosition or joints,
-                result)
+                       self._action_name,
+                       goal.component,
+                       goal.namedPosition or joints,
+                       result)
 
         return result == 3
 
@@ -175,16 +180,19 @@ class SunflowerAction(object):
 
         # step 0: check validity of parameters:
         if not isinstance(rotation, (int, float)) or not isinstance(linear, (int, float)):
-                rospy.logerr("Non-numeric rotation list, aborting moveBase")
-                return False
+            rospy.logerr("Non-numeric rotation list, aborting moveBase")
+            return False
         if abs(linear) >= maxTrans:
-                rospy.logerr("Maximal relative translation step exceeded(max: %sm), aborting moveBase" % maxTrans)
-                return False
+            rospy.logerr(
+                "Maximal relative translation step exceeded(max: %sm), aborting moveBase" % maxTrans)
+            return False
         if abs(rotation) >= maxRot:
-                rospy.logerr("Maximal relative rotation step exceeded(max: %srad), aborting moveBase" % maxRot)
-                return False
+            rospy.logerr(
+                "Maximal relative rotation step exceeded(max: %srad), aborting moveBase" % maxRot)
+            return False
 
-        # step 1: determine duration of motion so that upper thresholds for both translational as well as rotational velocity are not exceeded
+        # step 1: determine duration of motion so that upper thresholds for
+        # both translational as well as rotational velocity are not exceeded
         duration_trans_sec = abs(linear) / LINEAR_RATE
         duration_rot_sec = abs(rotation) / ROTATION_RATE
         duration_sec = max(duration_trans_sec, duration_rot_sec)
@@ -198,13 +206,15 @@ class SunflowerAction(object):
         twist = Twist()
         twist.linear.x = x_vel
         twist.angular.z = rot_vel
-        duration_ros = rospy.Duration.from_sec(duration_sec)  # duration of motion in ROS time
+        # duration of motion in ROS time
+        duration_ros = rospy.Duration.from_sec(duration_sec)
 
         r = rospy.Rate(50)
         end_time = rospy.Time.now() + duration_ros
         self._cmdVel.publish(twist)
         while not rospy.is_shutdown() and rospy.Time.now() < end_time:
-            # pub.publish(twist) #p2os has issues if you republish, seems to continue using last received cmd_vel
+            # pub.publish(twist) #p2os has issues if you republish, seems to
+            # continue using last received cmd_vel
             if self._as.is_preempt_requested():
                 rospy.loginfo('%s: Preempted' % self._action_name)
                 self._as.set_preempted()
@@ -234,8 +244,8 @@ class SunflowerAction(object):
 
         client.wait_for_server()
         rospy.loginfo("%s: Navigating to %s",
-                self._action_name,
-                pose)
+                      self._action_name,
+                      pose)
 
         handle = _ActionHandle(client)
         client.send_goal(client_goal)
@@ -267,105 +277,122 @@ class SunflowerAction(object):
         return handle.result
 
 
-#------------------- action_handle section -------------------#
-# Subscriber handle class.
-#
-# The subscriber handle is used to implement asynchronous behaviour within the script.
+# ------------------- action_handle section ------------------- #
 class _SubscriberHandle(object):
+    # Subscriber handle class.
+    #
+    # The subscriber handle is used to implement asynchronous behaviour within
+    # the script.
+
+    def __init__(self, subscribers, publishers):
         # Initialises the action handle.
-        def __init__(self, subscribers, publishers):
-            self._subscribers = subscribers
-            self._publishers = publishers
-            self._waiting = False
-            self._result = None
+        self._subscribers = subscribers
+        self._publishers = publishers
+        self._waiting = False
+        self._result = None
 
-        @property
-        def result(self):
-            if self._waiting:
-                return None
+    @property
+    def result(self):
+        if self._waiting:
+            return None
 
-            return self._result
+        return self._result
 
-        def wait(self, duration=None):
-            t = self.waitAsync(duration)
-            t.join()
+    def wait(self, duration=None):
+        t = self.waitAsync(duration)
+        # An attempt to fix parallel action issue
+        # t.join()
+        # Potential race condition, isAlive returns false before starting
+        time.sleep(0.01)
+        while t.isAlive():
+            time.sleep(0.001)
 
-        def waitAsync(self, duration=None):
-            thread = Thread(target=self._wait_for_finished, args=(duration,))
-            thread.setDaemon(True)
-            thread.start()
-            return thread
+    def waitAsync(self, duration=None):
+        thread = Thread(target=self._wait_for_finished, args=(duration,))
+        thread.setDaemon(True)
+        thread.start()
+        return thread
 
-        def _wait_for_finished(self, duration):
-            self._waiting = True
-            reached = True
-            while True:
-                r = rospy.Rate(100)
-                for sub in self._subscribers:
-                    if rospy.is_shutdown():
-                        return False
-
-                    while not sub.hasNewMessage:
-                        r.sleep()
-
-                    if sub.lastMessage.is_moving:
-                        continue
-
-                    reached &= abs(sub.lastMessage.error) < 0.03
-                    if not reached:
-                        rospy.logwarn("%s unable to reach %s.  Error value: %s" % (sub.topic, sub.lastMessage.goal_pos, sub.lastMessage.error))
-
-                    self._subscribers.remove(sub)
-
-                if not self._subscribers:
-                    break
-
-            self._result = 3 if reached else 4
-
-        def cancel(self):
+    def _wait_for_finished(self, duration):
+        self._waiting = True
+        reached = True
+        while True:
+            r = rospy.Rate(100)
             for sub in self._subscribers:
-                self._publishers[sub.topic].publish(Float64(sub.lastMessage.current_pos))
+                if rospy.is_shutdown():
+                    return False
 
-#------------------- action_handle section -------------------#
-# # Action handle class.
-#
-# The action handle is used to implement asynchronous behaviour within the script.
+                while not sub.hasNewMessage:
+                    r.sleep()
+
+                if sub.lastMessage.is_moving:
+                    continue
+
+                reached &= abs(sub.lastMessage.error) < 0.03
+                if not reached:
+                    rospy.logwarn("%s unable to reach %s.  Error value: %s" % (
+                        sub.topic, sub.lastMessage.goal_pos, sub.lastMessage.error))
+
+                self._subscribers.remove(sub)
+
+            if not self._subscribers:
+                break
+
+        self._result = 3 if reached else 4
+
+    def cancel(self):
+        for sub in self._subscribers:
+            self._publishers[sub.topic].publish(
+                Float64(sub.lastMessage.current_pos))
+
+
+# ------------------- action_handle section ------------------- #
 class _ActionHandle(object):
+    # Action handle class.
+    #
+    # The action handle is used to implement asynchronous behaviour within the
+    # script.
+
+    def __init__(self, simpleActionClient):
         # Initialises the action handle.
-        def __init__(self, simpleActionClient):
-            self._client = simpleActionClient
-            self._waiting = False
-            self._result = None
+        self._client = simpleActionClient
+        self._waiting = False
+        self._result = None
 
-        @property
-        def result(self):
-            if self._waiting:
-                return None
+    @property
+    def result(self):
+        if self._waiting:
+            return None
 
-            return self._result
+        return self._result
 
-        def wait(self, duration=None):
-            t = self.waitAsync(duration)
-            t.join()
+    def wait(self, duration=None):
+        t = self.waitAsync(duration)
+        # An attempt to fix parallel action issue
+        # t.join()
+        # Potential race condition, isAlive returns false before starting
+        time.sleep(0.01)
+        while t.isAlive():
+            time.sleep(0.001)
 
-        def waitAsync(self, duration=None):
-            thread = Thread(target=self._wait_for_finished, args=(duration,))
-            thread.setDaemon(True)
-            thread.start()
-            return thread
+    def waitAsync(self, duration=None):
+        thread = Thread(target=self._wait_for_finished, args=(duration,))
+        thread.setDaemon(True)
+        thread.start()
+        return thread
 
-        def _wait_for_finished(self, duration):
-            self._waiting = True
-            if duration is None:
-                self._client.wait_for_result()
-            else:
-                self._client.wait_for_result(rospy.Duration(duration))
+    def _wait_for_finished(self, duration):
+        self._waiting = True
+        if duration is None:
+            self._client.wait_for_result()
+        else:
+            self._client.wait_for_result(rospy.Duration(duration))
 
-            self._result = self._client.get_state()
-            self._waiting = False
+        self._result = self._client.get_state()
+        self._waiting = False
 
-        def cancel(self):
-            self._client.cancel_all_goals()
+    def cancel(self):
+        self._client.cancel_all_goals()
 
 
 class RosSubscriber(object):
@@ -396,11 +423,12 @@ class RosSubscriber(object):
 
     def _touch(self):
         self._lastAccess = time.time()
-        if self._subscriber == None:
-            self._subscriber = rospy.Subscriber(self._topic, self._dataType, self._callback)
+        if self._subscriber is None:
+            self._subscriber = rospy.Subscriber(
+                self._topic, self._dataType, self._callback)
 
     def unregister(self):
-        if self._subscriber != None:
+        if self._subscriber is not None:
             self._subscriber.unregister()
             self._subscriber = None
 
